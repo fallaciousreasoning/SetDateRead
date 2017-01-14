3e0acf48f20631b67f2af8bd12bf03f8ae50d1e4
Jay Harris <jay.harris@outlook.co.nz>
2017-01-14 21:04:55 +1300

    Initial project setup

---
+++ GoodreadsReadDates/Config.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodreadsReadDates
+{
+    public class Config
+    {
+        public const string CONFIG_FILE = "config.json";
+
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+
+        public Config Load()
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+    }
+}
