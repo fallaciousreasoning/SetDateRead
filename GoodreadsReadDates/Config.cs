4ce6ed2c091bbd99a20647c305c05c5a85fc6f2d
Jay Harris <jay.harris@outlook.co.nz>
2017-01-14 21:41:03 +1300

    Gets the basic program working

--- GoodreadsReadDates/Config.cs
+++ GoodreadsReadDates/Config.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace GoodreadsReadDates
 {
@@ -10,17 +12,23 @@ namespace GoodreadsReadDates
     {
         public const string CONFIG_FILE = "config.json";
 
+        [JsonProperty("api_key")]
         public string ApiKey { get; set; }
+        [JsonProperty("api_secret")]
         public string ApiSecret { get; set; }
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+        [JsonProperty("access_token_secret")]
+        public string AccessTokenSecret { get; set; }
 
-        public Config Load()
+        public static Config Load()
         {
-
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(CONFIG_FILE));
         }
 
         public void Save()
         {
-
+            File.WriteAllText(CONFIG_FILE, JsonConvert.SerializeObject(this));
         }
     }
 }
--- GoodreadsReadDates/Program.cs
+++ GoodreadsReadDates/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Goodreads;
 
 namespace GoodreadsReadDates
 {
@@ -10,6 +11,43 @@ namespace GoodreadsReadDates
     {
         static void Main(string[] args)
         {
+            var task = Run();
+            task.Wait();
+        }
+
+        static async Task Run()
+        {
+            var config = Config.Load();
+
+            var client = new GoodreadsClient(config.ApiKey, config.ApiSecret, config.AccessToken,
+                config.AccessTokenSecret);
+
+            var userId = await client.Users.GetAuthenticatedUserId();
+
+            var reviews = await client.Reviews.GetListByUser(userId.Value, shelfName: "read", pageSize: 1000);
+
+            var undated = reviews.List.Where(r => !r.DateRead.HasValue).ToList();
+            Console.WriteLine($"{undated.Count} books do not have a read date!");
+
+            foreach (var review in undated)
+            {
+                Console.Write($"Would you like to set the read date on {review.Book.Title} to {review.DateAdded}? (y/n) ");
+                string text;
+                while (!new[] { "y", "n" }.Contains(text = Console.ReadLine()))
+                    continue;
+
+                if (text == "n") continue;
+
+                var result = await client.Reviews.Edit(review.Id, dateRead: review.DateAdded);
+                Console.WriteLine((result ? "Set" : "Failed to set") + $" the read date on {review.Book.Title} to {review.DateAdded}.");
+            }
+
+            var shelves = await client.Shelves.GetListOfUserShelves(userId.Value);
+            var readShelf = shelves.List.First(shelf => shelf.Name == "read");
+
+            
+
+            Console.ReadKey();
         }
     }
 }
