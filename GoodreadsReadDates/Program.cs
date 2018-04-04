1d2ae81ebdbd3567ae8925329710585fff9723f4
Jay Harris <jay.harris@outlook.co.nz>
2018-04-04 20:56:50 +1000

    Updates the Goodreads client to fix the tool

--- GoodreadsReadDates/Program.cs
+++ GoodreadsReadDates/Program.cs
@@ -19,13 +19,12 @@ namespace GoodreadsReadDates
         {
             var config = Config.Load();
 
-            var client = new GoodreadsClient(config.ApiKey, config.ApiSecret, config.AccessToken,
-                config.AccessTokenSecret);
+            var client  = GoodreadsClient.CreateAuth(config.ApiKey, config.ApiSecret, config.AccessToken, config.AccessTokenSecret);
 
             var userId = await client.Users.GetAuthenticatedUserId();
 
             //TODO actually work with the pagination api
-            var reviews = await client.Reviews.GetListByUser(userId.Value, shelfName: "read", pageSize: 1000);
+            var reviews = await client.Reviews.GetListByUser(userId, shelfName: "read", pageSize: 1000);
 
             var undated = reviews.List.Where(r => !r.DateRead.HasValue).ToList();
             Console.WriteLine($"{undated.Count} books do not have a read date!");
@@ -39,11 +38,19 @@ namespace GoodreadsReadDates
 
                 if (text == "n") continue;
 
-                var result = await client.Reviews.Edit(review.Id, dateRead: review.DateAdded);
-                Console.WriteLine((result ? "Set" : "Failed to set") + $" the read date on {review.Book.Title} to {review.DateAdded}.");
+                try
+                {
+                    var result = await client.Reviews.Edit(review.Id, review.Body, review.Rating, review.DateAdded, "read");
+                    Console.WriteLine((result ? "Set" : "Failed to set") +
+                                      $" the read date on {review.Book.Title} to {review.DateAdded}.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Failed to set read date!");
+                }
             }
 
-            var shelves = await client.Shelves.GetListOfUserShelves(userId.Value);
+            var shelves = await client.Shelves.GetListOfUserShelves(userId);
             var readShelf = shelves.List.First(shelf => shelf.Name == "read");
 
             
